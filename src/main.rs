2f59a063fa7046339d20c24bd6edae768c888391
kashifulhaque <haque.kashiful7@gmail.com>
2023-12-18 22:41:21 +0530

    🛠️  Added two servers for API and Portal

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use actix_web::{ get, post, web, App, HttpResponse, HttpServer, Responder };
+use actix_web::{ get, post, App, HttpResponse, HttpServer, Responder };
+use futures::try_join;
 
 #[get("/")]
 async fn index() -> impl Responder {
@@ -10,19 +11,40 @@ async fn echo(req_body: String) -> impl Responder {
   HttpResponse::Ok().body(req_body)
 }
 
-async fn manual_index() -> impl Responder {
-  HttpResponse::Ok().body("Manually saying hi!")
+#[get("/api")]
+async fn api_index() -> impl Responder {
+  HttpResponse::Ok().body("Hello world from API")
+}
+
+#[post("/api/echo")]
+async fn api_echo(req_body: String) -> impl Responder {
+  HttpResponse::Ok().body(req_body)
+}
+
+#[get("/")]
+async fn portal_index() -> impl Responder {
+  HttpResponse::Ok().body("Portal goes here!")
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-  HttpServer::new(|| {
+  // Create the API server
+  let api_server = HttpServer::new(|| {
     App::new()
-      .service(index)
-      .service(echo)
-      .route("/api", web::get().to(manual_index))
+      .service(api_index)
+      .service(api_echo)
   })
-  .bind(("127.0.0.1", 8080))?
-  .run()
-  .await
+  .bind(("127.0.0.1", 9000))?;
+
+  // Create the portal server
+  let portal_server = HttpServer::new(|| {
+    App::new()
+      .service(portal_index)
+  })
+  .bind(("127.0.0.1", 9090))?;
+
+  // Start both the servers concurrently
+  try_join!(api_server.run(), portal_server.run())?;
+
+  Ok(())
 }
